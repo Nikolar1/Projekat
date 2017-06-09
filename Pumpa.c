92c8091b1ada8f99aeb73f90dbaf8e8d93f126d5
MilosMiki <miki2000milos@gmail.com>
2017-06-09 20:09:46 +0200

    Izmenjen fajl sa casa
    Sve sam pokvario :D

--- Pumpa.c
+++ Pumpa.c
@@ -9,6 +9,7 @@
 #include "Pumpa.h"
 #define MAX_PROC 100.0
 
+void PromeniCenu(Gorivo* p){
 stanjegoriva Dizel, Benzin, TNG;
 
 void PromeniCenu(stanjegoriva* p){
@@ -23,34 +24,89 @@ void DodajAkciju(stanjegoriva* p){
     printf("\nAkcija je uspesno izmenjena.\n");
 }
 
-void EvidencijaProdaje(char ImeGoriva[], float Litri,float Cena,float Akcija,float Ukupno,float Uplaceno,float Povracaj){
-    FILE* f;
-    f = fopen("EvidencijaProdaje.txt","a");
-    fprintf(f,"\r\n==========\r\n");
-    fprintf(f,"\n%s%f","Gorivo:",ImeGoriva);
-    fprintf(f,"\n%s%f","Litara:",Litri);
-    fprintf(f,"\n%s%f","Cena:",Cena);
-    fprintf(f,"\n%s%f","Akcija:",Akcija);
-    fprintf(f,"\n%s%f","Za uplatu:",Ukupno);
-    fprintf(f,"\n%s%f","Uplaceno:",Uplaceno);
-    fprintf(f,"\n%s%f\n","Povracaj:",Povracaj);
-    fclose(f);
+void Prodaja(){
+	stanjegoriva temp = PretragaGoriva();
+	FILE* f;
+    f = fopen("EvidencijaProdaje.dat","ab");
+	fwrite(&temp.Ime,sizeof(char),strlen(temp.Ime),f); //upis imena u fajl
+    float f1,f2;
+	do{
+    printf("Koliko litara goriva zelite prodati: ");
+    scanf("%f",&f1);
+	if(f1<temp.stanje){
+		printf("\nNemate dovoljno goriva na stanju. Imate samo %f litara. Pokusajte ponovo.\n",temp.stanje);
+	}
+	}while(f1<temp.stanje);
+    temp.stanje -= f1;
+	fwrite(&f1,sizeof(float),1,f); //upis litara u fajl
+    f1 *= temp.Cena;
+	fwrite(&f1,sizeof(float),1,f); //upis cene pre akcije u fajl
+	fwrite(&temp.akcija,sizeof(float),1,f); //upis akcije u fajl
+    printf("\nCena: %.2f",f1);
+    printf("\nAkcija: %f",temp.akcija);
+    f1 *= ((MAX_PROC - temp.akcija)/MAX_PROC);
+    printf("\nZa uplatu: %f",f1);
+	fwrite(&f1,sizeof(float),1,f); //upis cene posle akcije u fajl
+	do{
+		printf("\nKoliko dinara je uplaceno: ");
+		scanf("%f",&f2);
+		if(f2<f1){
+			printf("\nNije uplaceno dovoljno novca. Pokusajte ponovo.\n");
+		}
+	}while(f2<f1);
+	fwrite(&f2,sizeof(float),1,f); //upis uplacene kolicine novca u fajl
+    f2 -= f1;
+    printf("\nPovracaj: %f",f2);
+	fwrite(&f2,sizeof(float),1,f); //upis kolicine novca koju treba vratiti u fajl
+    printf("\nLitara na stanju: %f\n",temp.stanje);
+	fclose(f);
 }
 
-void Prodaja(stanjegoriva* p){
-    float Litri,Cena,Ukupno,Uplaceno,Povracaj;
-    printf("Koliko litara goriva zelite prodati: ");
-    scanf("%f",&Litri);
-    Cena = (p->Cena) * Litri;
-    printf("\nCena: %.2f",Cena);
-    printf("\nAkcija: %f",p->akcija);
-    Ukupno = (MAX_PROC - p->akcija) * Cena;
-    printf("\nZa uplatu: %f",Ukupno);
-    printf("\nKoliko dinara je uplaceno: ");
-    scanf("%f",&Uplaceno);
-    Povracaj = Uplaceno - Ukupno;
-    printf("\nPovracaj: %f",Povracaj);
-    EvidencijaProdaje(p->Ime,Litri,Cena,p->akcija,Ukupno,Uplaceno,Povracaj);
-    p->stanje -= Litri;
-    printf("\nLitara na stanju: %f\n",p->stanje);
+void reggorivo(){
+  printf("\tUnesite naziv novog goriva\n");
+  stanjegoriva temp;
+  scanf("%s",&temp.Ime[]);
+  printf("\tIdentifikacioni broj goriva (jedinstven ceo broj) \n");
+  scanf("%d",&temp.ID);
+  printf("\tUnesite cenu goriva \n");
+  scanf("%f",&temp.Cena);
+  printf("\tUnesite stanje goriva \n");
+  scanf("%f",&temp.stanje);
+  printf("\tUnesite akciju na gorivo \n");
+  scanf("%f",&temp.akcija);
+  FILE *f = fopen(GR_NAZIV,"a+b");
+  fwrite(&temp,sizeof(stanjegoriva),1,f);
+  fclose(f);
+}
+
+stanjegoriva PretragaGoriva(){
+  do{
+	  printf("\tUnesite naziv goriva kojem zelite da menjate stanje\n");
+  char naziv[STR_LEN];
+  scanf("%s",&naziv);
+  int temporary = 1;
+  stanjegoriva temp;
+  FILE *f = fopen(GR_NAZIV,"rb");
+  while (fread(&temp,sizeof(stanjegoriva),1,f)!=NULL){
+    fread(temp,sizeof(stanjegoriva),1,f);
+    if(strcmp(temp.ime,naziv)==0){
+      temporary=0;
+      break;
+    }
+  }
+  if(temporary==1){
+    printf("\tOdabrali ste nepostojece gorivo\n");
+  }
+  fclose(f);
+	}while(temporary!=0);
+  return temp;
+}
+
+void promenastanja(){
+	stanjegoriva temp = PretragaGoriva();
+    printf("Unesite novo stanje goriva\n");
+    scanf("%f",temp.stanje);
+    fseek(f,0-sizeof(stanjegoriva),SEEK_CUR);
+    fwrite(&temp,sizeof(stanjegoriva),1,f);
+  }
 }
